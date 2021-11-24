d0e35e7dca6fdea169ca90c0be5e717b429c02f3
glpalma <gustavolpalma@gmail.com>
2021-11-24 19:54:25 -0300

    addition of main functions' documentation

--- ex2/src/Rectangle.java
+++ ex2/src/Rectangle.java
@@ -8,13 +8,28 @@ public class Rectangle {
         this.yLower = yLowerLeft;
         this.yUpper = yUpperRight;
     }
-
+    
+    /**
+     * Checks if the rectangle object intersects with another rectangle.
+     * For two rectangles to be considered intersected, they need to have a portion of their areas overlapping.
+     * If they have opposite vertical sides (e.g. one's upper and the other one's lower) on the same coordinate
+     * and the same for the horizontal sides, the intersection is considered.
+     * @throws NullPointerException if the other rectangle is a null pointer
+     * @param other represents the other rectangle to be tested
+     * @return true if they intersect; false otherwise
+     */
     public boolean intersects(Rectangle other) {
         checkNullity(other);
         return (this.containsVerticalSideOf(other) && this.containsHorizontalSideOf(other)) ||
                (other.containsVerticalSideOf(this) && other.containsHorizontalSideOf(this));
     }
 
+    /**
+     * Checks if the rectangle object contains at least one vertical side from the other rectangle between its own vertical sides.
+     * @throws NullPointerException if the other rectangle is a null pointer
+     * @param other represents the other rectangle to be tested
+     * @return true if the current rectangle object contains the other rectangle's left or right sides between its own left and right sides.
+     */
     private boolean containsVerticalSideOf(Rectangle other) {
         checkNullity(other);
         boolean containsLeftSide = (xLeft <= other.xLeft) && (other.xLeft <= xRight);
@@ -23,6 +38,12 @@ public class Rectangle {
         return containsLeftSide || containsRightSide;
     }
 
+    /**
+     * Checks if the rectangle object contains at least one horizontal side from the other rectangle between its own horizontal sides.
+     * @throws NullPointerException if the other rectangle is a null pointer
+     * @param other represents the other rectangle to be tested
+     * @return true if the current rectangle object contains the other rectangle's lower or upper sides between its own lower and upper sides.
+     */
     private boolean containsHorizontalSideOf(Rectangle other) {
         checkNullity(other);
         boolean containsLowerSide = (yLower <= other.yLower) && (other.yLower <= yUpper);
@@ -31,9 +52,14 @@ public class Rectangle {
         return containsLowerSide || containsUpperSide;
     }
 
-    private void checkNullity(Rectangle t) {
-        if (t == null) {
-            throw new NullPointerException("The other triangle can't be null!");
+    /**
+     * Checks if the given rectangle t is null
+     * @throws NullPointerException if the given rectangle is a null pointer
+     * @param r represents the rectangle to be checked
+     */
+    private void checkNullity(Rectangle r) {
+        if (r == null) {
+            throw new NullPointerException("The other rectangle can't be null!");
         }
     }
 }
